befbc62192cc34a4aa9415a6c49819b409319d8a
Nayan Shah <nayan16rocks@gmail.com>
2020-02-25 18:11:19 +0530

    Update react-hello-world.js

--- react-hello-world.js
+++ react-hello-world.js
@@ -1,4 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+System.register(["react", "react-dom"], function (_export, _context) {
+  "use strict";
 
-ReactDOM.render(<button>A button created by React</button>, document.getElementById('react-root'));
+  var React, ReactDOM;
+  return {
+    setters: [function (_react) {
+      React = _react.default;
+    }, function (_reactDom) {
+      ReactDOM = _reactDom.default;
+    }],
+    execute: function () {
+      ReactDOM.render(React.createElement("button", null, "A button created by React"), document.getElementById('react-root'));
+    }
+  };
+});
