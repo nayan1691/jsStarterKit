065475a1c30741805ebd0bbcb52461ce847a4c37
Nayan Shah <nayan16rocks@gmail.com>
2020-02-25 19:20:29 +0530

    Update react-hello-world1.js

--- react-hello-world1.js
+++ react-hello-world1.js
@@ -1,4 +1,4 @@
-System.register(["react", "react-dom"], function (_export, _context) {
+System.register(["react", "react-dom", "vp"], function (_export, _context) {
   "use strict";
 
   var React, ReactDOM, TextInput;
@@ -7,11 +7,10 @@ System.register(["react", "react-dom"], function (_export, _context) {
       React = _react.default;
     }, function (_reactDom) {
       ReactDOM = _reactDom.default;
+    }, function (_vp) {
+      TextInput = _vp.TextInput;
     }],
     execute: function () {
-      ({
-        TextInput
-      } = require('./main.bundle'));
       ReactDOM.render(React.createElement(React.Fragment, null, React.createElement("button", null, "A button created by React"), React.createElement(TextInput, null)), document.getElementById('react-root'));
     }
   };
