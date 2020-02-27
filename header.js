00c2064ad270b101bdf87d3d355e11d63ed8d88b
Nayan Shah <nayan16rocks@gmail.com>
2020-02-27 12:11:30 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,15 +1,28 @@
-System.register(["react", "react-dom"], function (_export, _context) {
+System.register(["systemjs-webpack-interop", "react", "react-dom"], function (_export, _context) {
   "use strict";
 
-  var React, ReactDOM;
+  var setPublicPath, React, ReactDOM, lifecycles, bootstrap, mount, unmount;
   return {
-    setters: [function (_react) {
+    setters: [function (_systemjsWebpackInterop) {
+      setPublicPath = _systemjsWebpackInterop.setPublicPath;
+    }, function (_react) {
       React = _react.default;
     }, function (_reactDom) {
       ReactDOM = _reactDom.default;
     }],
     execute: function () {
-      ReactDOM.render(React.createElement(React.Fragment, null, React.createElement("h1", null, "Header")), document.getElementById('react-root'));
+      setPublicPath('header');
+      lifecycles = singleSpaReact({
+        React,
+        ReactDOM,
+        rootComponent: () => React.createElement("h1", null, "Header")
+      });
+
+      _export("bootstrap", bootstrap = lifecycles.bootstrap);
+
+      _export("mount", mount = lifecycles.mount);
+
+      _export("unmount", unmount = lifecycles.unmount);
     }
   };
 });
