706bffcdc155ded0af97fc1f0a99ff4b4f0e8ae1
Nayan Shah <nayan16rocks@gmail.com>
2020-02-27 11:49:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,21 @@
-import * as singleSpa from 'single-spa'
+System.register(["single-spa"], function (_export, _context) {
+  "use strict";
 
-singleSpa.registerApplication('vat', () => SystemJS.import('vat'), location => {
-            console.log(location.pathname);
-            return location.pathname.startsWith('/vat');
-          })
-singleSpa.registerApplication('it', () => SystemJS.import('it'), location => {
-            console.log(location.pathname);
-            return location.pathname.startsWith('/it');
-          })
-
-singleSpa.start()
+  var singleSpa;
+  return {
+    setters: [function (_singleSpa) {
+      singleSpa = _singleSpa;
+    }],
+    execute: function () {
+      singleSpa.registerApplication('vat', () => SystemJS.import('vat'), location => {
+        console.log(location.pathname);
+        return location.pathname.startsWith('/vat');
+      });
+      singleSpa.registerApplication('it', () => SystemJS.import('it'), location => {
+        console.log(location.pathname);
+        return location.pathname.startsWith('/it');
+      });
+      singleSpa.start();
+    }
+  };
+});
