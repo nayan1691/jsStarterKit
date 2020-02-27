149432f458a854bc55993f3870e1b997fd75f737
Nayan Shah <nayan16rocks@gmail.com>
2020-02-27 12:15:22 +0530

    Update config2.js

--- config2.js
+++ config2.js
@@ -7,9 +7,9 @@ System.register(["single-spa"], function (_export, _context) {
       singleSpa = _singleSpa;
     }],
     execute: function () {
-      singleSpa.registerApplication('header', () => SystemJS.import('header'), location => location.pathname.startsWith('/'));
       singleSpa.registerApplication('vat', () => SystemJS.import('vat'), location => location.pathname.startsWith('/vat'));
       singleSpa.registerApplication('it', () => SystemJS.import('it'), location => location.pathname.startsWith('/it'));
+      singleSpa.registerApplication('header', () => SystemJS.import('header'), location => location.pathname.startsWith('/'));
       singleSpa.start();
     }
   };
