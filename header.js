825d91eecb703399d90adb832a198f5e77cfc2aa
Nayan Shah <nayan16rocks@gmail.com>
2020-02-27 11:58:30 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,15 +1,15 @@
-System.register(["react"], function (_export, _context) {
+System.register(["react", "react-dom"], function (_export, _context) {
   "use strict";
 
-  var React, Header;
+  var React, ReactDOM;
   return {
     setters: [function (_react) {
       React = _react.default;
+    }, function (_reactDom) {
+      ReactDOM = _reactDom.default;
     }],
     execute: function () {
-      Header = () => React.createElement("h2", null, "Header");
-
-      _export("default", Header);
+      ReactDOM.render(React.createElement(React.Fragment, null, React.createElement("h1", null, "Header")), document.getElementById('react-root'));
     }
   };
 });
